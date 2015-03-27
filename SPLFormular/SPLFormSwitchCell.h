57e7ea799c9fb9a1d2fd67d7206df42badfabc24
Oliver Letterer <oliver.letterer@gmail.com>
2015-03-27 19:45:22 +0100

    Licensing

--- SPLFormular/SPLFormSwitchCell.h
+++ SPLFormular/SPLFormSwitchCell.h
@@ -1,6 +1,5 @@
-/**
- SPLManagedObjectFormViewController
- Copyright (c) 2014 Oliver Letterer <oliver.letterer@gmail.com>, Sparrow-Labs
+/*
+ Copyright (c) 2015 Oliver Letterer <oliver.letterer@gmail.com>, Sparrow-Labs
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
