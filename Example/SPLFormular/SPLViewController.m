d27843f683af12d3bfda12497050c677f39e4af1
Oliver Letterer <oliver.letterer@gmail.com>
2015-03-27 17:16:56 +0100

    Adds behavior composition

--- Example/SPLFormular/SPLViewController.m
+++ Example/SPLFormular/SPLViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "SPLViewController.h"
+#import "ManagedObject.h"
+#import "CoreDataStack.h"
+
 #import <SPLFormular/SPLFormular.h>
 
 @interface TestObject : NSObject
@@ -47,14 +50,21 @@
 
 @implementation SPLViewController
 
-#pragma mark - setters and getters
-
-#pragma mark - Initialization
-
 - (instancetype)init
 {
     if (self = [super initWithStyle:UITableViewStylePlain]) {
         TestObject *object = [TestObject new];
+
+        UITableViewCell *dataPrototype = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"dataPrototype"];
+        dataPrototype.selectionStyle = UITableViewCellSelectionStyleNone;
+
+        NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:NSStringFromClass([ManagedObject class])];
+        fetchRequest.sortDescriptors = @[ [NSSortDescriptor sortDescriptorWithKey:@"date" ascending:YES] ];
+
+        NSFetchedResultsController *controller = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
+                                                            managedObjectContext:[CoreDataStack sharedInstance].mainThreadManagedObjectContext
+                                                              sectionNameKeyPath:nil
+                                                                       cacheName:nil];
 
         SPLFormSection *section0 = [[SPLFormSection alloc] initWithName:@"Contact" fields:^NSArray *{
             return @[
@@ -66,21 +76,21 @@
 
         SPLFormSection *section1 = [[SPLFormSection alloc] initWithFields:^NSArray *{
             return @[
-                     [[SPLFormField alloc] initWithObject:object property:@selector(username) name:@"Username" type:SPLFormFieldTypeHumanText],
+//                     [[SPLFormField alloc] initWithObject:object property:@selector(username) name:@"Username" type:SPLFormFieldTypeHumanText],
                      [[SPLFormField alloc] initWithObject:object property:@selector(email) name:@"E-Mail" type:SPLFormFieldTypeEMail],
                      [[SPLFormField alloc] initWithObject:object property:@selector(password) name:@"Password" type:SPLFormFieldTypePassword],
                      [[SPLFormField alloc] initWithObject:object property:@selector(passwordConfirmation) name:@"Password confirmation" type:SPLFormFieldTypePassword],
                      [[SPLFormField alloc] initWithObject:object property:@selector(isHuman) name:@"I am a human" type:SPLFormFieldTypeBoolean],
-                     [[SPLFormField alloc] initWithObject:object property:@selector(hasHomepage) name:@"Homepage?" type:SPLFormFieldTypeBoolean],
-                     [[SPLFormField alloc] initWithObject:object property:@selector(homepage) name:@"Homepage" type:SPLFormFieldTypeURL],
+//                     [[SPLFormField alloc] initWithObject:object property:@selector(hasHomepage) name:@"Homepage?" type:SPLFormFieldTypeBoolean],
+//                     [[SPLFormField alloc] initWithObject:object property:@selector(homepage) name:@"Homepage" type:SPLFormFieldTypeURL],
                      ];
         }];
 
         SPLFormSection *section2 = [[SPLFormSection alloc] initWithName:@"Address" fields:^NSArray *{
             return @[
                      [[SPLFormField alloc] initWithObject:object property:@selector(street) name:@"Street" type:SPLFormFieldTypeHumanText],
-                     [[SPLFormField alloc] initWithObject:object property:@selector(zip) name:@"ZIP Code" type:SPLFormFieldTypeNumber],
-                     [[SPLFormField alloc] initWithObject:object property:@selector(city) name:@"City" type:SPLFormFieldTypeHumanText],
+//                     [[SPLFormField alloc] initWithObject:object property:@selector(zip) name:@"ZIP Code" type:SPLFormFieldTypeNumber],
+//                     [[SPLFormField alloc] initWithObject:object property:@selector(city) name:@"City" type:SPLFormFieldTypeHumanText],
                      [[SPLFormField alloc] initWithObject:object property:@selector(country) name:@"Country" type:SPLFormFieldTypeHumanText],
                      ];
         }];
@@ -98,8 +108,20 @@
                                      @"passwordConfirmation": [NSPredicate predicateWithFormat:@"password.length > 0"],
                                      };
 
+        SPLFetchedResultsBehavior *managedObjects = [[SPLFetchedResultsBehavior alloc] initWithPrototype:dataPrototype controller:controller configuration:^(UITableViewCell *cell, ManagedObject *object) {
+            cell.textLabel.text = object.name;
+            cell.detailTextLabel.text = [NSDateFormatter localizedStringFromDate:object.date dateStyle:NSDateFormatterMediumStyle timeStyle:NSDateFormatterShortStyle];
+        }];
+
         _formular = [[SPLFormular alloc] initWithObject:object sections:@[ section0, section1, section2 ] predicates:predicates];
-        _tableViewBehavior = [[SPLCompoundBehavior alloc] initWithFormular:_formular];
+        SPLCompoundBehavior *formBehavior = [[SPLCompoundBehavior alloc] initWithFormular:_formular];
+
+        _tableViewBehavior = [[SPLCompoundBehavior alloc] initWithBehaviors:@[
+                                                                              formBehavior,
+                                                                              [[SPLSectionBehavior alloc] initWithTitle:@"CoreData" behaviors:@[ managedObjects ]]
+                                                                              ]];
+
+        [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(_deleteRandomObjects) userInfo:nil repeats:YES];
     }
     return self;
 }
@@ -109,8 +131,6 @@
     [self doesNotRecognizeSelector:_cmd];
     return [self init];
 }
-
-#pragma mark - View lifecycle
 
 - (void)viewDidLoad
 {
@@ -122,8 +142,48 @@
 
     self.tableView.rowHeight = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? 88.0 : 66.0;
     self.tableView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
+
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(_add)];
 }
 
-#pragma mark - Private category implementation ()
+- (void)_add
+{
+    NSManagedObjectContext *context = [CoreDataStack sharedInstance].mainThreadManagedObjectContext;
+    static int count = 0;
+
+    for (int i = 0; i < arc4random_uniform(10); i++) {
+        count++;
+
+        ManagedObject *object = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([ManagedObject class])
+                                                              inManagedObjectContext:context];
+        object.name = [NSString stringWithFormat:@"CoreData Object %d", count];
+        object.date = [NSDate date];
+    }
+
+    NSError *saveError = nil;
+    [context save:&saveError];
+    NSCAssert(saveError == nil, @"error saving managed object context: %@", saveError);
+}
+
+- (void)_deleteRandomObjects
+{
+    NSManagedObjectContext *context = [CoreDataStack sharedInstance].backgroundThreadManagedObjectContext;
+
+    [context performBlock:^{
+        NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:NSStringFromClass([ManagedObject class])];
+        fetchRequest.sortDescriptors = @[ [NSSortDescriptor sortDescriptorWithKey:@"date" ascending:YES] ];
+
+        NSArray *objects = [context executeFetchRequest:fetchRequest error:NULL];
+
+        NSRange range = NSMakeRange(0, arc4random_uniform((u_int32_t)objects.count + 1));
+        [objects enumerateObjectsAtIndexes:[NSIndexSet indexSetWithIndexesInRange:range] options:kNilOptions usingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            [context deleteObject:obj];
+        }];
+
+        NSError *saveError = nil;
+        [context save:&saveError];
+        NSCAssert(saveError == nil, @"error saving managed object context: %@", saveError);
+    }];
+}
 
 @end
--- Podfile
+++ Podfile
@@ -3,6 +3,7 @@ use_frameworks!
 
 target 'SPLFormular', :exclusive => true do
   pod "SPLFormular", :path => "."
+  pod "SLCoreDataStack", "~> 0.8.0"
 end
 
 target 'Tests', :exclusive => true do
